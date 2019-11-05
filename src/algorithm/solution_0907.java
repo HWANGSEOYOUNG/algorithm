d02dc64fe632ad69986d23e95da8174b3c1eabb8
SEOYOUNG <seo089@naver.com>
2019-11-05 19:13:49 +0900

    2020 카카오 코테 1번문제 문자열압축

--- src/algorithm/solution_0907.java
+++ src/algorithm/solution_0907.java
@@ -1,32 +1,74 @@
 package algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class solution_0907 {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        System.out.println(solution("aabbaccc"));
+		System.out.println(solution("aabbaccc"));
 
-    }
+	}
 
-    public static int solution(String s){
-        int answer = 0;
+	public static int solution(String s) {
 
-        answer = s.length();
+        if (s.length() == 1) {
+            return 1;
+        }
 
-        int same1 = 0;
+        int answer = s.length();
+        int maxSize = s.length() / 2;
 
-        for(int i = 1; i<s.length();i++){
-            if(s.charAt(i-1)==s.charAt(i)){
-                same1++;
-                if(same1 >=2){
-                    answer--;
+        List<String> list = new ArrayList<String>();
+
+        int cnt = 1;
+        int chSize = 1;
+
+        while(chSize <= maxSize) {
+
+            for (int i = 0; i < s.length(); i+=chSize) {
+
+                if (i <= s.length() - chSize) {
+                    list.add(s.substring(i, i + chSize));
+                } else if(i < s.length()) {
+                    list.add(s.substring(i));
                 }
-            }else{
-                same1 = 0;
             }
+
+            String result = "";
+
+            for (int j = 1; j < list.size(); j++) {
+                if (list.get(j - 1).equals(list.get(j))) {
+                    cnt++;
+                } else {
+                    if (cnt > 1) {
+                        result += cnt + list.get(j-1);
+                    } else {
+                        result += list.get(j-1);
+                    }
+                    cnt = 1;
+                }
+
+                if(j == list.size()-1){
+                    if (cnt > 1) {
+                        result += cnt + list.get(j);
+                    } else {
+                        result += list.get(j);
+                    }
+                    cnt = 1;
+                }
+            }
+
+            if(answer> result.length()){
+                answer = result.length();
+            }
+            
+            list.clear();
+            chSize++;
+
         }
 
         return answer;
-    }
+	}
 }
